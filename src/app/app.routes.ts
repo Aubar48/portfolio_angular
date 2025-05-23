bf80a5d798ad9f52d48cecaa13340169d2f62d7f
Aubar48 <rociio.fressa@gmail.com>
2025-05-23 14:04:09 -0300

    se agrego login, register, politicas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,16 @@
 import { Routes } from '@angular/router';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { RecuperarpwComponent } from './pages/recuperarpw/recuperarpw.component';
+import { PoliticasComponent } from './pages/politicas/politicas.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: '**', component: NotFoundComponent },
+    { path: 'login', component: LoginComponent},
+    { path: 'register', component: RegisterComponent},
+    { path: 'recuperarpw', component: RecuperarpwComponent},
+    { path: 'politicas', component: PoliticasComponent},
+    { path: '**', component: NotFoundComponent }
 ];
---
+++ src/app/pages/recuperarpw/recuperarpw.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recuperarpw',
+  imports: [],
+  templateUrl: './recuperarpw.component.html',
+  styleUrl: './recuperarpw.component.scss'
+})
+export class RecuperarpwComponent {
+
+}
